b55ea1b6bba0574b9c533c9b91444016e10682ba
Dan Shemesh <dansheme@users.noreply.github.com>
2017-05-24 12:25:37 +0300

    Update install_agent.sh

--- install_agent.sh
+++ install_agent.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-wget -qO- https://github.com/Jumperr-labs/jumper-logging-agent/archive/0.0.10.tar.gz | tar xvz
-./jumper-logging-agent-0.0.10/install.sh
+wget -qO- https://github.com/Jumperr-labs/jumper-logging-agent/archive/0.0.11.tar.gz | tar xvz
+./jumper-logging-agent-0.0.11/install.sh
 
