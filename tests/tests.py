8db41c8b58c26a6add520249c1e266b21f676398
Jonathan Seroussi <seryoni@gmail.com>
2017-05-29 11:47:33 +0300

    moving to datetime

--- tests/tests.py
+++ tests/tests.py
@@ -4,7 +4,8 @@ import unittest
 import os
 import json
 import struct
-import time
+from datetime import datetime
+import pytz
 from jumper_ble_logger.event_parser_middleware import EventParser
 from jumper_ble_logger.ble_logger import change_dictionary_keys_from_str_to_int
 
@@ -13,8 +14,7 @@ VALID_CONFIG_FILE = os.path.join(ROOT_DIR, 'events_config.json')
 DEFAULT_MAC = 'AA:BB:CC:DD:EE:FF'
 
 
-def create_packet(timestamp=None, version=1, event_type=2, data_length=1, data=b'\x01'):
-    timestamp = timestamp or round(time.time())
+def create_packet(timestamp=0, version=1, event_type=2, data_length=1, data=b'\x01'):
     return struct.pack('<BBIB{}s'.format(data_length), version, event_type, timestamp, data_length, data)
 
 
@@ -47,26 +47,26 @@ class EventParserTests(unittest.TestCase):
         self.assertRaises(ValueError, EventParser, invalid_config)
 
     def test_sanity(self):
-        timestamp = int(round(time.time()))
-        packet = create_packet(timestamp)
-        event_dict = self.events_parser.parse(DEFAULT_MAC, packet, 0)
+        timestamp = datetime.now(pytz.utc)
+        packet = create_packet(timestamp=0)
+        event_dict = self.events_parser.parse(DEFAULT_MAC, packet, timestamp)
 
         expected_dict = dict(timestamp=timestamp, type='ADVERTISING_STATE', is_on=1, device_id=DEFAULT_MAC)
         self.assertDictEqual(expected_dict, event_dict)
 
     def test_boot_event(self):
-        timestamp = int(round(time.time()))
+        timestamp = datetime.now(pytz.utc)
         data = b'v1.1.2\x00'
-        packet = create_packet(timestamp=timestamp, version=1, event_type=0, data_length=len(data), data=data)
-        event_dict = self.events_parser.parse(DEFAULT_MAC, packet, 0)
+        packet = create_packet(timestamp=0, version=1, event_type=0, data_length=len(data), data=data)
+        event_dict = self.events_parser.parse(DEFAULT_MAC, packet, timestamp)
         expected_dict = dict(timestamp=timestamp, type='DEVICE_BOOT', version='v1.1.2', device_id=DEFAULT_MAC)
         self.assertDictEqual(expected_dict, event_dict)
 
     def test_string_event(self):
-        timestamp = int(round(time.time()))
+        timestamp = datetime.now(pytz.utc)
         data = b'HELLO\x00'
-        packet = create_packet(timestamp=timestamp, version=1, event_type=5, data_length=len(data), data=data)
-        event_dict = self.events_parser.parse(DEFAULT_MAC, packet, 0)
+        packet = create_packet(timestamp=0, version=1, event_type=5, data_length=len(data), data=data)
+        event_dict = self.events_parser.parse(DEFAULT_MAC, packet, timestamp)
         expected_dict = dict(timestamp=timestamp, type='CUSTOM_STRING', custom_string='HELLO', device_id=DEFAULT_MAC)
         self.assertDictEqual(expected_dict, event_dict)
 
@@ -77,4 +77,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
